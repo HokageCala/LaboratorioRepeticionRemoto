11e66f038b2b9eb7eb3483acf64f91e97c0ceb50
Cala Torra <000404667@bga.upb>
2019-03-01 13:39:06 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,6 +1,26 @@
-a=int(input("ingrese el numero"))
+positivos=0
+cero=0
+negativos=0
 
-if a > 0:
-    print("numero es mayor que cero")
-else:
-    print("el numero es menor de cero")
+
+for x in range(0,10):
+    x=int(input("ingrese un numero "))
+    if x==0:
+            cero=cero + 1
+            
+    else:
+        if x>0:
+            positivos= positivos + 1
+           
+        else:
+            negativos= negativos + 1
+        
+
+print("la cantidad de ceros es: " , cero)
+print("la cantidad de positivos es: " , positivos)
+print("la cantidad de negativos es: " , negativos)
+
+
+
+
+
