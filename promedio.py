64fb2542105b43a4d07f97548b26b7c052a48980
Cala Torra <000404667@bga.upb>
2019-03-01 14:56:29 -0500

    tercer ejercio

---
+++ promedio.py
@@ -0,0 +1,8 @@
+
+for a in range(0,5):
+    a=int(input("ingrese la nota"))
+    n=a
+    suma= (n+n+n+n+n)
+    promedio= (suma/5)
+
+print("la suma de las notas es: {} y el promedio es {}" .format(suma,promedio))
